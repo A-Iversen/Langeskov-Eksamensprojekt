3a7b97245f77350e3199177d47451ef54f182516
Peter8977 <Peter.loevebo@gmail.com>
2025-11-06 10:48:27 +0100

    Restructure MVVM folders and add RelayCommand
    Moved MainWindow files into the View folder and updated namespaces and StartupUri accordingly. Added Core/RelayCommand.cs for command handling and created an empty MainViewModel in the ViewModel folder. Cleaned up MVVM.csproj folder includes to match new structure.

---
+++ Langeskov-Eksamensprojekt/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.Core;
+
+namespace MVVM.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+    }
+}
