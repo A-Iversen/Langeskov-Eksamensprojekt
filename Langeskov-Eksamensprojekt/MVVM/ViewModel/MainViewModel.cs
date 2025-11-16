b69f04d37b6e926a75287c3a928f83d926d95505
Peter8977 <Peter.loevebo@gmail.com>
2025-11-16 22:25:06 +0100

    Add user controls and navigation for Member and Accounting views
    Introduced MemberControl and AccountingControl user controls with corresponding XAML and code-behind files. Updated MainViewModel to support navigation between these views using RelayCommands. Refactored MainWindow layout and styles to accommodate the new navigation structure and improved UI consistency, including new color scheme and button style adjustments.

--- Langeskov-Eksamensprojekt/MVVM/ViewModel/MainViewModel.cs
+++ Langeskov-Eksamensprojekt/MVVM/ViewModel/MainViewModel.cs
@@ -5,13 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using MVVM.Core;
+using MVVM.View.UserControls;
 
 namespace MVVM.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
 
+		private object _currentView = new MemberControl();
 
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value;
+				OnPropertyChanged();
+			}
+		}
 
-    }
+		public RelayCommand ShowMemberViewCommand => new RelayCommand(execute => CurrentView = new MemberControl());
+		public RelayCommand ShowAccountingViewCommand => new RelayCommand(execute => CurrentView = new AccountingControl());
+
+	}
 }
