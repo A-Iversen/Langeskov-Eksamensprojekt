3a818e384f0344c615260d88222ccf15c4460dd1
Peter8977 <Peter.loevebo@gmail.com>
2025-12-10 23:28:07 +0100

    Mange UI fixes
    -Fixed Combobox Spacing (Ved at fjerne andet)
    -Added Resizing
    -Fixed Maximize Screen
    -Minimeret Datagridets sortings knapper pladsbrug.
    -Fixet width ændring ved calender når der skiftes fra måned til dag, samt tilføjning af fødselsdag text for at visse hvad elementet udvælger.
    -Tilføjet farve ved accounting navigations Ribbon.

--- Langeskov-Eksamensprojekt/MVVM/View/MainWindow.xaml.cs
+++ Langeskov-Eksamensprojekt/MVVM/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,19 +21,34 @@ namespace MVVM.View
         {
             InitializeComponent();
             DataContext = new MainViewModel();
-
+            
         }
+
 
         private void MinimizeClick(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
 
+        /* Maximere vinduet og rekreaere noget "margin" igennem padding omkring vinduet, 
+         samt fjernelse af corner radius på visse UI elementer så det maksimerede vindue ikke er zoomed ind. */
         private void MaximizeClick(object sender, RoutedEventArgs e)
         {
             if (this.WindowState == WindowState.Normal)
+            {
                 this.WindowState = WindowState.Maximized;
-            else this.WindowState = WindowState.Normal;
+                MainWindowBackgroundBorder.Padding = new Thickness(10,5,10,10);
+                MainWindowBackgroundBorder.CornerRadius = new CornerRadius(0);
+                MainWindowLogoBorder.CornerRadius = new CornerRadius(0, 0, 39, 10);
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                MainWindowBackgroundBorder.Padding = new Thickness(3, 0, 6, 6);
+                MainWindowBackgroundBorder.CornerRadius = new CornerRadius(20);
+                MainWindowLogoBorder.CornerRadius = new CornerRadius(20, 0, 39, 10);
+            }
+
         }
 
         private void CloseClick(object sender, RoutedEventArgs e)
@@ -42,6 +59,69 @@ namespace MVVM.View
         private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+
+        //Vindue Resizeing
+        private const int HTLEFT = 10;
+        private const int HTRIGHT = 11;
+        private const int HTTOP = 12;
+        private const int HTTOPLEFT = 13;
+        private const int HTTOPRIGHT = 14;
+        private const int HTBOTTOM = 15;
+        private const int HTBOTTOMLEFT = 16;
+        private const int HTBOTTOMRIGHT = 17;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseCapture();
+
+        private void ResizeBorderTop_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+
+                e.Handled = true; // Prevent other events from firing
+
+                var border = sender as FrameworkElement;
+                IntPtr windowHandle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+
+                // 3. Define the correct message constant
+                const int WM_NCLBUTTONDOWN = 0xA1;
+
+                if (border != null)
+                {
+                    switch (border.Name)
+                    {
+                        case "ResizeBorderLeft":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTLEFT, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderRight":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTRIGHT, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderTop":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTTOP, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderBottom":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTBOTTOM, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderTopLeft":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTTOPLEFT, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderTopRight":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTTOPRIGHT, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderBottomLeft":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTBOTTOMLEFT, IntPtr.Zero);
+                            break;
+                        case "ResizeBorderBottomRight":
+                            SendMessage(windowHandle, WM_NCLBUTTONDOWN, (IntPtr)HTBOTTOMRIGHT, IntPtr.Zero);
+                            break;
+                    }
+                }
+            }
         }
     }
 }
